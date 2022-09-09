f7e925ba20f80069af05468f8acae45ede7d6ca2
Febri <audirmdhnii@gmail.com>
2022-09-09 14:47:38 +0700

    Tugas4

---
+++ DPW_Tugas4/system/app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthController extends Controller
+{
+    function showLogin()
+    {
+        return view('admin.login');
+    }
+    function processLogin()
+    {
+    }
+    function logout()
+    {
+    }
+    function registration()
+    {
+    }
+    function forgotPassword()
+    {
+    }
+}
